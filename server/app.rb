e52b89f36834299a41580c2477109cf4f012efcd
Mark Cerqueira <mark.cerqueira@gmail.com>
2017-01-14 16:10:30 -0800

    Only render a file once and keep resources in a different folder

--- server/app.rb
+++ server/app.rb
@@ -18,7 +18,7 @@ get '/resource/result/:key/?' do
 end
 
 
-OUTPUT_FOLDER = ''
+OUTPUT_FOLDER = 'resources/'
 LIMIT_DURATION = 1
 DURATION_LIMIT_SECONDS = 30
 
@@ -27,37 +27,41 @@ AAC_EXTENSION = '.m4a'
 
 # Main render function that takes source code (params key 'source') or a file (params key 'file')
 # and renders the file in ChucK and converts that file from .wav to .m4a
-post '/render/?' do
-  output_filename = "#{SecureRandom.hex}"
-    
-  WAV_FILE = "#{OUTPUT_FOLDER}#{output_filename}#{WAV_EXTENSION}"
-  AAC_FILE = "#{OUTPUT_FOLDER}#{output_filename}#{AAC_EXTENSION}"
-    
-  # Write our temporary file to disk so we can pass it to ChucK below  
-  File.open("#{output_filename}.ck", "w") do |f|
+post '/render/?' do    
+  # Write our temporary file or source to disk so we can pass it to ChucK below
+  chuck_filename = "#{OUTPUT_FOLDER}#{SecureRandom.hex}.ck"
+  File.open(chuck_filename, "w") do |f|
     if !params['source'].nil? && params['source'].to_s.strip.length > 0
       f.write(params['source'])
     elsif
       f.write(params['file'][:tempfile].read)
-    end  
+    end
   end
-  
-  # Generate .wav file with export.ck script
-  `chuck --silent export.ck:"#{output_filename}.ck":#{WAV_FILE}:#{LIMIT_DURATION}:#{DURATION_LIMIT_SECONDS}`
 
-  # Convert .wav file to .m4a. More ffmpeg conversion: http://superuser.com/a/370637
-  `ffmpeg -i #{WAV_FILE} -c:a libfdk_aac -vbr 4 #{AAC_FILE}`
+  # Use SHA256 digest of file contents because we may have rendered this file already
+  output_filename = Digest::SHA256.hexdigest File.read(chuck_filename)
   
+  wav_file = "#{OUTPUT_FOLDER}#{output_filename}#{WAV_EXTENSION}"
+  aac_file = "#{OUTPUT_FOLDER}#{output_filename}#{AAC_EXTENSION}"
+  
+  if !File.exist?(aac_file)
+    # Generate .wav file with export.ck script
+    `chuck --silent export.ck:"#{chuck_filename}":#{wav_file}:#{LIMIT_DURATION}:#{DURATION_LIMIT_SECONDS}`
+
+    # Convert .wav file to .m4a. More ffmpeg conversion: http://superuser.com/a/370637
+    `ffmpeg -i #{wav_file} -c:a libfdk_aac -vbr 4 #{aac_file}`
+  end
+    
   redirect "/resource/result/#{output_filename}/"
 end
 
 # Downloads .m4a resource for resource with given key
 get '/resource/download/:key/?' do
-  AAC_FILE = "#{OUTPUT_FOLDER}#{params[:key]}#{AAC_EXTENSION}"
+  aac_file = "#{OUTPUT_FOLDER}#{params[:key]}#{AAC_EXTENSION}"
   begin
-    attachment AAC_FILE
+    attachment aac_file
     content_type 'application/octet-stream'
-    File.read(AAC_FILE)
+    File.read(aac_file)
   rescue StandardError => error
     error.message
   end
